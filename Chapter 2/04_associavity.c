bd095c091dd9d96cd331b9e6ac7f75998b50cba6
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-22 19:24:23 +0530

    associavity

---
+++ Chapter 2/04_associavity.c
@@ -0,0 +1,15 @@
+//
+// Created by steve on 22-08-2025.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a = 4605;
+    int b = 95434;
+    int c = 12345;
+
+    printf("The value is %d\n", a*b/c + 7896);
+    printf("The value is %d", 465444*b/2*566 + 453*a);
+    return 0;
+}
