e49af119bc1e475d0450f5006898a0fe5951fdc6
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-22 19:28:50 +0530

    typecasting

---
+++ Chapter 2/05_typecasting.c
@@ -0,0 +1,14 @@
+//
+// Created by steve on 22-08-2025.
+//
+\
+#include <stdio.h>
+
+int main() {
+    int n = 54;
+    float m = 56.74;
+
+    n = (int) m; //convert the data type to int
+    printf("%d\n", n);
+    return 0;
+}
