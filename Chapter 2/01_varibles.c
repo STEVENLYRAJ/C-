7363de197a2728b003be3979346f20c2e770050d
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-22 19:22:59 +0530

    variables

---
+++ Chapter 2/01_varibles.c
@@ -0,0 +1,14 @@
+//
+// Created by steve on 22-08-2025.
+//
+
+#include <stdio.h>
+
+int main() {
+    int i = 844523; // Declare and initialize 'i' is 10
+    int j = 123456;
+    // %d if called format specifier
+    // %d is for int, %f is for float and %c is for char
+    printf("The value of i is %d and value of j is %d", i, j);
+    return 0;
+}
