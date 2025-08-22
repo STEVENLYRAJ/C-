e44505715092d444522d1be31713902a62b59480
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-22 17:13:47 +0530

    comments

---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,16 @@
+//
+// Created by steve on 22-08-2025.
+//
+
+/*
+program: 05_comments.c
+Author: Steven
+Date: 22 August 2025
+*/
+#include <stdio.h>
+
+int main() {
+     // I am writing this for fun. This line does not do anything.
+    printf("Hello World\n");
+    return 0; // This is return
+}
