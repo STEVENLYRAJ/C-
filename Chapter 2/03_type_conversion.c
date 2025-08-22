a2414608dba8008608a70d16a8279ab5e9c56aed
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-22 19:24:07 +0530

    type conversion

---
+++ Chapter 2/03_type_conversion.c
@@ -0,0 +1,15 @@
+//
+// Created by steve on 22-08-2025.
+//
+
+#include <stdio.h>
+
+int main() {
+    float a = 55.6;
+    int b = 6;
+    float c = 55.6/6;
+    int d = 546596754.59475496849;
+    printf("The value of a/b is %f\n", c);
+    printf("The value of d is %d", d);
+    return 0;
+}
