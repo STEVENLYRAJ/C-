dc13fa57fe350933f121ac3f87487ee52d6c7f59
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-22 17:13:59 +0530

    inputs

---
+++ Chapter 1/06_inputs.c
@@ -0,0 +1,20 @@
+//
+// Created by steve on 22-08-2025.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    float b;
+    char c;
+
+    scanf("%d", &a);
+    scanf("%f", &b);
+    scanf("%c", &c);
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+    return 0;
+}
