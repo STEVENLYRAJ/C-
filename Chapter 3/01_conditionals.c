893dc1aa61f1754fa5997f5998c21d8dafc287d2
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-28 20:50:31 +0530

    conditionals

---
+++ Chapter 3/01_conditionals.c
@@ -0,0 +1,20 @@
+//
+// Created by steve on 28-08-2025.
+//
+
+#include <stdio.h>
+
+int main() {
+    int age = 15;
+
+    if (age>10) {
+        printf("We are inside if.\n");
+        printf("Your age is greater than 10\n");
+    }
+    if (age%5==0){
+        printf("We are inside another if.\n");
+        printf("Your age is greater than 10\n");
+    }
+    return 0;
+
+}
