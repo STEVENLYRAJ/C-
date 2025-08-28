7ce67f7f4f2ec6e6be996b0d27cec9c8c508a3e4
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-08-28 20:52:14 +0530

    ternary

---
+++ Chapter 3/06_ternary.c
@@ -0,0 +1,13 @@
+//
+// Created by steve on 28-08-2025.
+//
+
+#include <stdio.h>
+
+int main () {
+    // conditions ? expression-if-true : expression-if-false
+    int a = 4556;
+    int b = 453557;
+    a>b?printf("a is greater"):printf("b is greater");
+    return 0;
+}
