1c5206315aa6a239bc05af73edfdd9a8aa814538
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-09-17 19:10:06 +0530

    quiz

---
+++ Chapter 4/05_quiz.c
@@ -0,0 +1,16 @@
+//
+// Created by steve on 10-09-2025.
+//
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while (i<=20) {
+        if (i>=10) {
+            printf("The value of i is %d\n", i);
+        }
+        i++;
+    }
+
+    return 0;
+}
