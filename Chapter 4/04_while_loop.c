9ec9338a24e505c73b3e2ec18eaea09a3454f2a6
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-09-17 19:09:28 +0530

    while loop

---
+++ Chapter 4/04_while_loop.c
@@ -0,0 +1,16 @@
+//
+// Created by steve on 10-09-2025.
+//
+
+#include <stdio.h>
+
+int main () {
+
+    int i = 0;
+    while (i<10) {
+        printf("The value of i is %d\n", i);
+        // i++; // since i increment is commented and it won't be print if this code run therefor i will print infinite times
+
+    }
+    return 0;
+}
