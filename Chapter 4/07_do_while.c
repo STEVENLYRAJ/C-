72277e9861a56053e52189c52a6a7d2667d8af17
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-09-17 19:11:38 +0530

    do while

---
+++ Chapter 4/07_do_while.c
@@ -0,0 +1,14 @@
+//
+// Created by steve on 17-09-2025.
+//
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    do {
+        printf("The value int is %d\n", i);
+        i++;
+    } while (i<4);
+
+    return 0;
+}
