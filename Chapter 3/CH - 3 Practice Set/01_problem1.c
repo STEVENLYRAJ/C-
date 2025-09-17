0e6893630d380bed414be783d8902ed58a0f9d55
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-09-17 19:07:24 +0530

    problem1

---
+++ Chapter 3/CH - 3 Practice Set/01_problem1.c
@@ -0,0 +1,15 @@
+//
+// Created by steve on 06-09-2025.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    if (a = 11)
+        printf("I am 11");
+    else
+        printf("I am not 11");
+    return 0;
+}
