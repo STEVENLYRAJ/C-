94ca55c1bb1fb90bf1f9870990bdddad60102f24
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-09-17 19:08:20 +0530

    while value

---
+++ Chapter 4/03_while_value.c
@@ -0,0 +1,17 @@
+//
+// Created by steve on 10-09-2025.
+//
+
+
+#include <stdio.h>
+
+int main () {
+
+    int i = 0;
+    while (i<10) {
+        printf("The value of i is %d\n", i);
+        i++;
+
+    }
+    return 0;
+}
