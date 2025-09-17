a9a43ed93433efbf43dd9d0440d1945999e64d69
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-09-17 19:07:49 +0530

    while loop

---
+++ Chapter 4/02_while.c
@@ -0,0 +1,15 @@
+//
+// Created by steve on 10-09-2025.
+//
+
+#include <stdio.h>
+
+int main () {
+
+    int i = 0;
+    while (i<5) {
+        printf("Happy Birthday!\n"); i++
+        // i = i + 1
+    }
+    return 0;
+}
