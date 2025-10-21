ba98c6af21d14178bf569b5488a6b6c0c00df090
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-10-21 19:13:43 +0530

    quiz

---
+++ Chapter 4/12_quiz.c
@@ -0,0 +1,15 @@
+//
+// Created by steve on 17-09-2025.
+//
+
+
+#include <stdio.h>
+
+int main() {
+    int n = 35;
+    for (int i = n; i ; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
