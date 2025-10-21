45b320710740b1483f20142f2d5d8fc6c8ce3bcc
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-10-21 19:14:38 +0530

    quick quiz

---
+++ Chapter 5/02_quick_quiz.c
@@ -0,0 +1,27 @@
+//
+// Created by steve on 07-10-2025.
+//
+#include <stdio.h>
+
+void good_morning();
+void good_afternoon();
+void good_evening();
+void good_night();
+
+void good_morning() {
+    printf("good morning\n");
+}
+void good_afternoon() {
+    printf("good afternoon\n");
+}
+void good_evening() {
+    printf("good evening\n");
+}
+void good_night() {
+    printf("good night\n");
+}
+
+int main() {
+
+    return 0;
+}
