6f93a4d6dd50cdb4ec54205dfffdbc9bbea264f5
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-10-22 19:09:43 +0530

    Modification of 3rd Problem(ch-6)

--- Chapter 6/Ch - 6 Practice Set/03_problem3.c
+++ Chapter 6/Ch - 6 Practice Set/03_problem3.c
@@ -3,3 +3,14 @@
 //
 #include <stdio.h>
 
+int change_to_thirty_times(int* a) {
+    *a = *a * 30;
+}
+
+int main() {
+    int s = 75;
+    printf("Value of s is %d\n", s);
+    change_to_thirty_times(&s);
+    printf("Value of s is %d\n", s);
+    return 0;
+}
