f4790ffcb121caa6976ff1eeeac3b5ea03701f1e
Raj Shekhar <62145321+STEVENLYRAJ@users.noreply.github.com>
2025-10-26 01:51:29 +0530

    arrays

---
+++ Chapter 7/01_arrays.c
@@ -0,0 +1,16 @@
+//
+// Created by steve on 25-10-2025.
+//
+#include <stdio.h>
+
+int main() {
+    int marks[90];
+
+    marks[0] = 10;
+    marks[1] = 20;
+    marks[2] = 30;
+    // We can go all the way till marks[89]
+
+    printf("Marks 0, Marks 1, and Marks 2 is %d %d %d", marks[0], marks[1], marks[2]);
+    return 0;
+}
